224364039ea23ce4c1ef37662b4d59c80cd3bb8a
wpqlks7 <wpqlks7@gmail.com>
2026-01-03 01:12:12 +0900

    setting blue/green ci/cd

---
+++ src/main/java/io/github/jjh030325/cicd/CicdApplication.java
@@ -0,0 +1,13 @@
+package io.github.jjh030325.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdApplication.class, args);
+    }
+
+}
